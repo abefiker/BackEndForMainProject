063b61a4088de0f4b4fd2c023fe455a1ca355fb9
abfiker <new94abemelek@gmail.com>
2024-03-14 21:08:40 +0300

    "change on mongoose connection"

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,11 +1,26 @@
-const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://127.0.0.1:27017/BackendForMainProject")
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb://127.0.0.1:27017/', {
+    dbName: 'BackendForMainProject'
+})
 .then(()=>{
     console.log("mongogb connected");
 })
-.catch(()=>{
-    console.log("Failed to connect");
+.catch((err)=>console.log(err))
+
+mongoose.connection.on('connected',()=>{
+    console.log("mongoose connected to db")
+})
+mongoose.connection.on('error',(err)=>{
+    console.log(err.message)
+})
+mongoose.connection.on('disconnected',()=>{
+    console.log("mongoose connection is disconnected.")
+})
+process.on('SIGINT',async()=>{
+    await mongoose.Connection.close()
+    process.exit(0)
 })
 
 const LogInSchema = new mongoose.Schema({
