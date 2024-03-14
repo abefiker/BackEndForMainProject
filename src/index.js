15fa8f69fe141b65d1a45a222299901a227e4a96
abfiker <new94abemelek@gmail.com>
2024-03-14 20:34:00 +0300

    "error handling and some routes added"

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const AuthRoute = require("../Routes/Auth.Route")
 const app = express();
 const morgan = require("morgan")
 const createError = require("http-errors")
@@ -19,53 +20,67 @@ const publicPath = path.join(__dirname, '../public');
 app.set('view engine', 'hbs');
 app.set('views', templatePath);
 app.use(express.static(publicPath));
+
+
 app.get("/", (req, res) => {
-  res.render('login');
+  res.render('hello from express');
 });
+app.use('/auth',AuthRoute)
 
 
+// app.get("/signup", (req, res) => {
+//   res.render('signup');
+// });
 
-app.get("/signup", (req, res) => {
-  res.render('signup');
-});
+// app.post("/signup", async (req, res) => {
+//   const data = {
+//     firstname: req.body.firstname,
+//     lastname: req.body.lastname,
+//     email: req.body.email,
+//     password: req.body.password
+//   };
+//   const check = await collection.findOne({ email: req.body.email })
 
-app.post("/signup", async (req, res) => {
-  const data = {
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
-    email: req.body.email,
-    password: req.body.password
-  };
-  const check = await collection.findOne({ email: req.body.email })
+//   if (check) {
+//     return res.status(400).send("User details already exist");
+//   }
 
-  if (check) {
-    return res.status(400).send("User details already exist");
-  }
+//   await collection.insertMany([data]);
+//   res.status(201).render('home', {
+//     naming: req.body.firstname,
+//     lnaming: req.body.lastname
+//   });
+// });
 
-  await collection.insertMany([data]);
-  res.status(201).render('home', {
-    naming: req.body.firstname,
-    lnaming: req.body.lastname
-  });
-});
+// app.post("/login", async (req, res) => {
+//   try {
+//     const check = await collection.findOne({ email: req.body.email });
+//     if (check && check.password === req.body.password) {  // Use strict comparison (===)
+//       res.status(200).render('home', {
+//         naming: check.firstname,
+//         lnaming: check.lastname
+//       });
+//     } else {
+//       res.send('Invalid credentials'); // More informative message
+//     }
+//   } catch (error) {
+//     console.error("Error during login:", error);
+//     res.status(500).send('Internal server error'); // Generic error for client
+//   }
+// });
+// app.use(async (req,res,next)=>{
+//   next(createError.NotFound())
+// })
 
-app.post("/login", async (req, res) => {
-  try {
-    const check = await collection.findOne({ email: req.body.email });
-    if (check && check.password === req.body.password) {  // Use strict comparison (===)
-      res.status(200).render('home', {
-        naming: check.firstname,
-        lnaming: check.lastname
-      });
-    } else {
-      res.send('Invalid credentials'); // More informative message
-    }
-  } catch (error) {
-    console.error("Error during login:", error);
-    res.status(500).send('Internal server error'); // Generic error for client
-  }
-});
-
+// app.use((err,req,res,next)=>{
+//   res.status(err.status || 500),
+//   res.send({
+//     error :{
+//       status : err.status || 500,
+//       message : err.message
+//     }
+//   })
+// })
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
