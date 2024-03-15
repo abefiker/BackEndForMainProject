d76a8c12019a54f6bcb8480fe37c55a587f292ed
abfiker <new94abemelek@gmail.com>
2024-03-15 10:23:19 +0300

    "gitingnore added"

--- src/jwt.helper.js
+++ src/jwt.helper.js
@@ -5,7 +5,7 @@ module.exports = {
     signAccessToken: (userid) => {
         return new Promise((resolve, reject) => {
             const payload = {}
-            const secret = "My Super secret Key"
+            const secret = process.env.ACCESS_TOKEN_SECRET
             const options = {
                 expiresIn: '1h',
                 issuer:'https://oasis-lovat.vercel.app/',
